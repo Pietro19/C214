97ea757a9f81c79fac3bc7cd2eb9d01435fc39a9
Pietro19 <piscardoso15@gmail.com>
2023-09-13 13:56:35 -0300

    add desafio05

--- Laboratorio/Aula05/src/imc.js
+++ Laboratorio/Aula05/src/imc.js
@@ -4,7 +4,29 @@ function calcularIMC(peso, altura) {
   }
 
   const alturaMetros = altura / 100; // Converte altura de centímetros para metros
-  return (peso / (alturaMetros * alturaMetros)).toFixed(2); // Calcula o IMC com duas casas decimais
+  const imc = (peso / (alturaMetros * alturaMetros)).toFixed(2); // Calcula o IMC com duas casas decimais
+
+  let mensagem = '';
+
+  if (imc < 16) {
+    mensagem = 'Magreza grave';
+  } else if (imc >= 16 && imc < 17) {
+    mensagem = 'Magreza moderada';
+  } else if (imc >= 17 && imc < 18.5) {
+    mensagem = 'Magreza leve';
+  } else if (imc >= 18.5 && imc < 24.9) {
+    mensagem = 'Peso saudável';
+  } else if (imc >= 25 && imc < 29.9) {
+    mensagem = 'Sobrepeso';
+  } else if (imc >= 30 && imc < 34.9) {
+    mensagem = 'Obesidade grau I';
+  } else if (imc >= 35 && imc < 39.9) {
+    mensagem = 'Obesidade grau II (severa)';
+  } else {
+    mensagem = 'Obesidade grau III (mórbida)';
+  }
+
+  return `IMC: ${imc} - ${mensagem}`;
 }
 
-module.exports = { calcularIMC }; // Exporta a função calcularIMC
+module.exports = { calcularIMC };
--- Laboratorio/Aula05/test/imc.test.js
+++ Laboratorio/Aula05/test/imc.test.js
@@ -9,7 +9,8 @@ describe('Calculadora de IMC', () => {
     const IMEsperado = (peso / Math.pow(altura / 100, 2)).toFixed(2)
     const IMCCalculado = calcularIMC(peso, altura)
 
-    expect(IMCCalculado).to.equal(IMEsperado)
+    // Use expect para comparar o valor calculado com o valor esperado
+    expect(IMCCalculado).to.equal(`IMC: ${IMEsperado} - Peso saudável`)
   })
 
   it('altura = 0, entrada inadequada', () => {
@@ -19,5 +20,25 @@ describe('Calculadora de IMC', () => {
     const IMCCalculado = calcularIMC(peso, altura)
 
     expect(IMCCalculado).to.equal('NaN')
+  })
+
+  it('Calcula IMC e retorna mensagem de interpretação', () => {
+    const peso = 70 // 70 kg
+    const altura = 180 // 180 cm
+
+    const IMC = calcularIMC(peso, altura)
+
+    // Verifica se a mensagem de interpretação está correta
+    expect(IMC).to.equal('IMC: 21.60 - Peso saudável')
+  })
+
+  it('Calcula IMC para caso de obesidade mórbida', () => {
+    const peso = 150 // 150 kg
+    const altura = 160 // 160 cm
+
+    const IMC = calcularIMC(peso, altura)
+
+    // Verifica se a mensagem de interpretação está correta
+    expect(IMC).to.equal('IMC: 58.59 - Obesidade grau III (mórbida)')
   })
 })
